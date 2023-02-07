ef7ab7f7b1c0efc2326ceeee55794aa3b98785ec
Claudio V <claudiov.dejesus@protonmail.com>
2023-02-07 06:19:59 -0300

    move settings to proper folder

--- init.lua
+++ init.lua
@@ -146,7 +146,7 @@ require("lazy").setup({
 -- [[ Setting options ]]
 -- See `:help vim.o`
 
-require("settings")
+require("user/settings")
 
 vim.keymap.set("n", "zR", require("ufo").openAllFolds)
 vim.keymap.set("n", "zM", require("ufo").closeAllFolds)
