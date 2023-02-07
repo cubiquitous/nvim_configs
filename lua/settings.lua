3662fe78d1f01573a574c74a1271fcdfa6fd103b
Claudio V <claudiov.dejesus@protonmail.com>
2023-02-07 06:17:24 -0300

    add a few extra configs

--- lua/settings.lua
+++ lua/settings.lua
@@ -1,7 +1,7 @@
-local opt = vim.opt
+-- local opt = vim.opt
 local set = vim.o
 
-opt.background = "dark" -- set this to dark or light
+set.background = "dark" -- set this to dark or light
 
 set.foldcolumn = "1" -- '0' is not bad
 set.foldlevel = 99 -- Using ufo provider need a large value, feel free to decrease the value
@@ -29,7 +29,16 @@ vim.wo.signcolumn = "yes"
 set.clipboard = "unnamedplus"
 -- Set colorscheme
 set.completeopt = "menuone,noselect"
-
+set.cursorline = true
+set.expandtab = true -- tabs will become spaces
+set.fileencoding = "utf-8"
+set.splitbelow = true -- splits forced to be below
+set.splitright = true -- splits forced to be at right
+set.shiftwidth = 2 -- amount of spaces at each indent
+set.tabstop = 2 -- spaces for each tab
+set.scrolloff = 5 --number of lines shown above and below the cursor
+set.autoread = true -- automatically reloads the file if isn't already
+set.confirm = true
 --[[
   avaliable themes: 
      oxocarbon
@@ -38,6 +47,9 @@ set.completeopt = "menuone,noselect"
 vim.cmd.colorscheme("oxocarbon")
 set.termguicolors = true
 set.t_Co = 256
+
+-- "-" is interpred as it's own keyword
+vim.opt.iskeyword:append("-")
 
 -- leader key
 vim.g.mapleader = " "
