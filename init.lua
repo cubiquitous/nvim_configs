bb927964dcb57486b0a2a197405d327667b46f20
Claudio V <claudiov.dejesus@protonmail.com>
2023-02-07 21:20:36 -0300

    move extras to it's own file

--- init.lua
+++ init.lua
@@ -4,44 +4,6 @@ vim.cmd([[ let g:neo_tree_remove_legacy_commands = 1 ]])
 require("user/plugins")
 require("user/settings")
 
--- from u/m00fin_
-vim.cmd([[
-	let g:skipview_files = [
-		\ '[EXAMPLE PLUGIN BUFFER]'
-		\ ]
-	function! MakeViewCheck()
-		if has('quickfix') && &buftype =~ 'nofile'
-			" Buffer is marked as not a file
-			return 0
-		endif
-		if empty(glob(expand('%:p')))
-			" File does not exist on disk
-			return 0
-		endif
-		if index(g:skipview_files, expand('%')) >= 0
-			" File is in skip list
-			return 0
-		endif
-		return 1
-	endfunction
-	augroup vimrcAutoView
-		autocmd!
-		" Autosave & Load Views.
-		autocmd BufWritePost,BufLeave,BufWinLeave,WinLeave ?* if MakeViewCheck() | mkview! | endif
-		autocmd BufWinEnter ?* if MakeViewCheck() | normal! zx
-		autocmd BufWinEnter,BufWritePost ?* if MakeViewCheck() | silent! loadview | endif
-	augroup end
-]])
-
-local highlight_group = vim.api.nvim_create_augroup("YankHighlight", { clear = true })
-vim.api.nvim_create_autocmd("TextYankPost", {
-	callback = function()
-		vim.highlight.on_yank()
-	end,
-	group = highlight_group,
-	pattern = "*",
-})
-
 -- Turn on lsp status information
 require("fidget").setup()
 
@@ -96,20 +58,8 @@ cmp.setup({
 	},
 })
 
--- creates small delay until the dropdown shows up
-vim.cmd([[
-  let s:timer = 0
-  autocmd TextChangedI * call s:on_text_changed()
-  function! s:on_text_changed() abort
-    call timer_stop(s:timer)
-    let s:timer = timer_start(250, function('s:complete'))
-  endfunction
-  function! s:complete(...) abort
-    lua require('cmp').complete({ reason = require('cmp').ContextReason.Auto })
-  endfunction
-]])
-
 require("user/lsp")
 require("user/mappings")
+require("user/others")
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
---
+++ lua/user/others.lua
@@ -0,0 +1,52 @@
+-- from u/m00fin
+-- creates persistent foldings and saves cursor location_
+vim.cmd([[
+	let g:skipview_files = [
+		\ '[EXAMPLE PLUGIN BUFFER]'
+		\ ]
+	function! MakeViewCheck()
+		if has('quickfix') && &buftype =~ 'nofile'
+			" Buffer is marked as not a file
+			return 0
+		endif
+		if empty(glob(expand('%:p')))
+			" File does not exist on disk
+			return 0
+		endif
+		if index(g:skipview_files, expand('%')) >= 0
+			" File is in skip list
+			return 0
+		endif
+		return 1
+	endfunction
+	augroup vimrcAutoView
+		autocmd!
+		" Autosave & Load Views.
+		autocmd BufWritePost,BufLeave,BufWinLeave,WinLeave ?* if MakeViewCheck() | mkview! | endif
+		autocmd BufWinEnter ?* if MakeViewCheck() | normal! zx
+		autocmd BufWinEnter,BufWritePost ?* if MakeViewCheck() | silent! loadview | endif
+	augroup end
+]])
+
+-- highlight on yank
+local highlight_group = vim.api.nvim_create_augroup("YankHighlight", { clear = true })
+vim.api.nvim_create_autocmd("TextYankPost", {
+	callback = function()
+		vim.highlight.on_yank()
+	end,
+	group = highlight_group,
+	pattern = "*",
+})
+
+-- creates small delay until the autocomplete dropdown shows up
+vim.cmd([[
+  let s:timer = 0
+  autocmd TextChangedI * call s:on_text_changed()
+  function! s:on_text_changed() abort
+    call timer_stop(s:timer)
+    let s:timer = timer_start(250, function('s:complete'))
+  endfunction
+  function! s:complete(...) abort
+    lua require('cmp').complete({ reason = require('cmp').ContextReason.Auto })
+  endfunction
+]])
