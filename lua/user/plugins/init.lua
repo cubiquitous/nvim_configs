dd081fea35c5d9b4912a6f651d55a95bd7248a1e
Claudio V <claudiov.dejesus@protonmail.com>
2023-02-08 09:36:24 -0300

    import the files via specific functions

--- lua/user/plugins/init.lua
+++ lua/user/plugins/init.lua
@@ -12,263 +12,45 @@ end
 
 vim.opt.rtp:prepend(lazypath)
 
+--
+local function path_generator(main_path)
+	return function(sub_path)
+		return require(main_path .. sub_path)
+	end
+end
+
+--
+local colorscheme = path_generator("user/colorschemes/")
+local plugin = path_generator("user/plugins/")
+--
+
+--
 require("lazy").setup({
-	{
-		"echasnovski/mini.nvim",
-		config = function()
-			require("mini.pairs").setup()
-		end,
-	},
-	{
-		"rose-pine/neovim",
-		as = "rose-pine",
-	},
-	{
-		"nacro90/numb.nvim",
-		config = function()
-			require("numb").setup()
-		end,
-	},
-	{
-		"nvim-neo-tree/neo-tree.nvim",
-		branch = "v2.x",
-		dependencies = {
-			"nvim-lua/plenary.nvim",
-			"nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
-			"MunifTanjim/nui.nvim",
-		},
-	},
-
-	{
-		"kevinhwang91/nvim-ufo",
-		dependencies = "kevinhwang91/promise-async",
-		config = function()
-			require("ufo").setup({
-				provider_selector = function(bufnr, filetype, buftype)
-					return { "treesitter", "indent" }
-				end,
-			})
-		end,
-	},
-	"jose-elias-alvarez/null-ls.nvim",
-
-	{
-		"numToStr/Comment.nvim",
-		config = function()
-			require("Comment").setup()
-		end,
-	},
-
-	{
-		"kylechui/nvim-surround",
-		config = function()
-			require("nvim-surround").setup({})
-		end,
-	},
-
-	{ "romgrk/barbar.nvim", dependencies = "nvim-web-devicons" },
-	{ "nyoom-engineering/oxocarbon.nvim" },
-
-	{
-		"folke/todo-comments.nvim",
-		dependencies = "nvim-lua/plenary.nvim",
-		config = function()
-			require("todo-comments").setup({
-				-- your configuration comes here
-				-- or leave it empty to use the default settings
-				-- refer to the configuration section below
-			})
-		end,
-	},
-
-	{
-		"brenoprata10/nvim-highlight-colors",
-		config = function()
-			require("nvim-highlight-colors").setup({})
-		end,
-	},
-
-	{ "sindrets/diffview.nvim", dependencies = "nvim-lua/plenary.nvim" },
-
-	-- =================================================================================
-
-	{ -- LSP Configuration & Plugins
-		"neovim/nvim-lspconfig",
-		config = function()
-			require("neodev").setup()
-		end,
-		dependencies = {
-			-- Automatically install LSPs to stdpath for neovim
-			"williamboman/mason.nvim",
-			"williamboman/mason-lspconfig.nvim",
-
-			-- Useful status updates for LSP
-			{
-				"j-hui/fidget.nvim",
-				config = function()
-					require("fidget").setup()
-				end,
-			},
-
-			-- Additional lua configuration, makes nvim stuff amazing
-			"folke/neodev.nvim",
-		},
-	},
-
-	{ -- Autocompletion
-		"hrsh7th/nvim-cmp",
-		dependencies = {
-			"hrsh7th/cmp-nvim-lsp",
-			"L3MON4D3/LuaSnip",
-			"saadparwaiz1/cmp_luasnip",
-			"hrsh7th/cmp-buffer",
-			"hrsh7th/cmp-path",
-			"hrsh7th/cmp-nvim-lua",
-		},
-	},
-
-	{ -- Highlight, edit, and navigate code
-		"nvim-treesitter/nvim-treesitter",
-		config = function()
-			pcall(require("nvim-treesitter.install").update({ with_sync = true }))
-			require("nvim-treesitter.configs").setup({
-				-- Add languages to be installed here that you want installed for treesitter
-				ensure_installed = { "c", "cpp", "go", "lua", "python", "rust", "typescript", "help", "vim" },
-
-				highlight = { enable = true },
-				indent = { enable = true, disable = { "python" } },
-				incremental_selection = {
-					enable = true,
-					keymaps = {
-						init_selection = "<c-space>",
-						node_incremental = "<c-space>",
-						scope_incremental = "<c-s>",
-						node_decremental = "<c-backspace>",
-					},
-				},
-				textobjects = {
-					select = {
-						enable = true,
-						lookahead = true, -- Automatically jump forward to textobj, similar to targets.vim
-						keymaps = {
-							-- You can use the capture groups defined in textobjects.scm
-							["aa"] = "@parameter.outer",
-							["ia"] = "@parameter.inner",
-							["af"] = "@function.outer",
-							["if"] = "@function.inner",
-							["ac"] = "@class.outer",
-							["ic"] = "@class.inner",
-						},
-					},
-					move = {
-						enable = true,
-						set_jumps = true, -- whether to set jumps in the jumplist
-						goto_next_start = {
-							["]m"] = "@function.outer",
-							["]]"] = "@class.outer",
-						},
-						goto_next_end = {
-							["]M"] = "@function.outer",
-							["]["] = "@class.outer",
-						},
-						goto_previous_start = {
-							["[m"] = "@function.outer",
-							["[["] = "@class.outer",
-						},
-						goto_previous_end = {
-							["[M"] = "@function.outer",
-							["[]"] = "@class.outer",
-						},
-					},
-					swap = {
-						enable = true,
-						swap_next = {
-							["<leader>a"] = "@parameter.inner",
-						},
-						swap_previous = {
-							["<leader>A"] = "@parameter.inner",
-						},
-					},
-				},
-			})
-		end,
-	},
-
-	{ -- Additional text objects via treesitter
-		"nvim-treesitter/nvim-treesitter-textobjects",
-		after = "nvim-treesitter",
-	},
-
-	-- Git related plugins
-	"tpope/vim-fugitive",
-	"tpope/vim-rhubarb",
-	{
-		"lewis6991/gitsigns.nvim",
-		config = function()
-			require("gitsigns").setup({
-				signs = {
-					add = { text = "+" },
-					change = { text = "~" },
-					delete = { text = "_" },
-					topdelete = { text = "‾" },
-					changedelete = { text = "~" },
-				},
-			})
-		end,
-	},
-
-	"navarasu/onedark.nvim", -- Theme inspired by Atom
-	{
-		"nvim-lualine/lualine.nvim",
-		config = function()
-			require("lualine").setup({
-				options = {
-					icons_enabled = false,
-					theme = "rose-pine",
-					component_separators = "|",
-					section_separators = "",
-				},
-			})
-		end,
-	},
-	{
-		"lukas-reineke/indent-blankline.nvim",
-		config = function()
-			require("indent_blankline").setup({
-				char = "│",
-				show_trailing_blankline_indent = false,
-			})
-		end,
-	},
-	{
-		"numToStr/Comment.nvim",
-		config = function()
-			require("Comment").setup()
-		end,
-	},
-	"tpope/vim-sleuth", -- Detect tabstop and shiftwidth automatically
-
-	-- Fuzzy Finder (files, lsp, etc)
-	{
-		"nvim-telescope/telescope.nvim",
-		branch = "0.1.x",
-		dependencies = { "nvim-lua/plenary.nvim" },
-		config = function()
-			require("telescope").setup({
-				defaults = {
-					mappings = {
-						i = {
-							["<C-u>"] = false,
-							["<C-d>"] = false,
-						},
-					},
-				},
-			})
-
-			pcall(require("telescope").load_extension, "fzf")
-		end,
-	},
-
-	-- Fuzzy Finder Algorithm which requires local dependencies to be built. Only load if `make` is available
-	{ "nvim-telescope/telescope-fzf-native.nvim", run = "make", cond = vim.fn.executable("make") == 1 },
+	plugin("mini_nvim"),
+	plugin("numb_nvim"),
+	plugin("neotree"),
+	plugin("nvim_ufo"),
+	plugin("null_ls_nvim"),
+	plugin("comment_nvim"),
+	plugin("barbar"),
+	plugin("nvim_surround"),
+	plugin("todo_comments"),
+	plugin("nvim_highlight_colors"),
+	plugin("diffview"),
+	plugin("lsp_config"),
+	plugin("cmp"),
+	plugin("nvim_treesitter"),
+	plugin("treesitter_textobjects"),
+	plugin("vim-fugitive"),
+	plugin("rhubarb"),
+	plugin("tpope/vim-sleuth"),
+	plugin("gitsigns"),
+	plugin("lualine"),
+	plugin("indent-blankline"),
+	plugin("telescope"),
+	plugin("telescope-fzf-native"),
+	--
+	colorscheme("rose_pine"),
+	colorscheme("oxocarbon"),
+	colorscheme("onedark"),
 })
