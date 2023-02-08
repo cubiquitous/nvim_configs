f7e6d15df350e6f283481e15efe622a928b8d13b
Claudio V <claudiov.dejesus@protonmail.com>
2023-02-08 09:37:08 -0300

    remapped neotree and vim_diagnostics

--- lua/user/mappings.lua
+++ lua/user/mappings.lua
@@ -8,7 +8,7 @@ vim.keymap.set({ "n", "v" }, "<Space>", "<Nop>", { silent = true })
 vim.keymap.set("n", "k", "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
 vim.keymap.set("n", "j", "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
 
-vim.keymap.set("n", "<leader>fe", "<cmd>Neotree toggle<cr>", {})
+vim.keymap.set("n", "<leader>e", "<cmd>Neotree toggle <cr>", {})
 vim.keymap.set("n", "<leader>w", "<cmd>w<cr>", {})
 
 -- See `:help telescope.builtin`
@@ -32,7 +32,7 @@ vim.keymap.set("n", "<leader>sd", require("telescope.builtin").diagnostics, { de
 
 vim.keymap.set("n", "[d", vim.diagnostic.goto_prev)
 vim.keymap.set("n", "]d", vim.diagnostic.goto_next)
-vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float)
+vim.keymap.set("n", "<leader>ld", vim.diagnostic.open_float)
 vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist)
 
 local map = vim.keymap.set
