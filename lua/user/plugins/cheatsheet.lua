8bff017565e2a8347fb5c7dcf10ad98c806d0e67
Claudio V <claudiov.dejesus@protonmail.com>
2023-02-12 05:32:46 -0300

    CHANGE: improve cheatsheet

--- lua/user/plugins/cheatsheet.lua
+++ lua/user/plugins/cheatsheet.lua
@@ -1,5 +1,13 @@
 return {
 	"sudormrfbin/cheatsheet.nvim",
+	config = function()
+		require("cheatsheet").setup({
+			bundled_cheatsheets = {
+				disabled = { "unicode", "markdown", "regex" },
+			},
+			bundled_plugin_cheatsheets = false,
+		})
+	end,
 	dependencies = {
 		{ "nvim-telescope/telescope.nvim" },
 		{ "nvim-lua/popup.nvim" },
