6862c3022215261821ad71842319e1e15fb7bcab
Claudio V <claudiov.dejesus@protonmail.com>
2023-06-02 15:45:58 -0300

    add extra configs

--- lua/user/plugins/mini_nvim.lua
+++ lua/user/plugins/mini_nvim.lua
@@ -1,6 +1,18 @@
 return {
 	"echasnovski/mini.nvim",
 	config = function()
-		require("mini.pairs").setup()
+		local forEach = require("user.utils.loops").forEach
+		local comment = {
+			mappings = {
+				comment = "gc",
+				comment_line = "gc",
+				textobject = "gc",
+			},
+		}
+
+		local selections = { pairs = {}, ai = {}, surround = {}, comment = comment }
+		forEach(selections, function(k, v)
+			require("mini." .. k).setup(v)
+		end)
 	end,
 }
