a250f8305cacb7e7752b9be6df80bedf9b990417
Claudio V <claudiov.dejesus@protonmail.com>
2023-06-02 15:46:21 -0300

    move to variations of forEach instead of map

--- lua/user/utils/loops.lua
+++ lua/user/utils/loops.lua
@@ -1,8 +1,20 @@
 local M = {}
 
-M.map = function(arr, func)
-	for _, v in ipairs(arr) do
+M.forEachValue = function(arr, func)
+	for _, v in pairs(arr) do
 		func(v)
+	end
+end
+
+M.forEachKey = function(arr, func)
+	for k, _ in pairs(arr) do
+		func(k)
+	end
+end
+
+M.forEach = function(arr, func)
+	for k, v in pairs(arr) do
+		func(k, v)
 	end
 end
 
